ec767fac98a17e209900da4c03ccecc61908f78f
kn45 <381404825@qq.com>
2018-01-06 18:05:09 +0800

    rm redundant code

--- old_style.py
+++ old_style.py
@@ -1,6 +1,44 @@
+# -*- coding=utf8 -*-
+
 import common
+import conf
 import copy
-from game import GameStatus
+import random
+from itertools import product
+from game import *
+
+
+class Player(object):
+    def __init__(self, player_idx=None):
+        self.PLAYER = player_idx
+
+    def choose_best_move(self, game, *args):
+        raise NotImplementedError
+
+
+class TicTacToePlayer(Player):
+    def __init__(self, player_idx):
+        super(TicTacToePlayer, self).__init__(player_idx)
+        self.__search = search.MinMax(
+            evaluate.SimpleEndEval(),
+            max_depth=conf.tictactoe_minmax_depth)
+
+    def choose_best_move(self, game, *args):
+        return self.__search.search_best_move(game, eval_side=self.PLAYER)
+
+
+class GoBangPlayer(Player):
+    def __init__(self, player_idx):
+        super(GoBangPlayer, self).__init__(player_idx)
+        self.__search = search.MinMax(
+            evaluate.SimpleEndEval(),
+            max_depth=conf.gobang_minmax_depth)
+
+    def choose_best_move(self, game, *args):
+        common.dprint('considering...')
+        pos, value = self.__search.search_best_move(game, eval_side=self.PLAYER)
+        common.dprint('\t'.join(['my move:', common.pos2h(pos, game.board.width), str(value)]))
+        return pos, value
 
 
 class Search(object):
@@ -66,3 +104,34 @@ class MinMax(Search):
         res = self.__max_move(game, level=0, alpha=self.__MIN_VAL, beta=self.__MAX_VAL)
         common.dprint(str(res))
         return res
+
+
+class Evaluate(object):
+    def __init__(self, min_val, max_val):
+        self.__MIN_VAL = min_val
+        self.__MAX_VAL = max_val
+
+    @property
+    def MIN_VAL(self):
+        return self.__MIN_VAL
+
+    @property
+    def MAX_VAL(self):
+        return self.__MAX_VAL
+
+    def evaluate(self, **kwargs):
+        raise NotImplementedError
+
+
+class SimpleEndEval(Evaluate):
+    def __init__(self):
+        super(SimpleEndEval, self).__init__(-1, +1)
+
+    def evaluate(self, **kwargs):
+        game = kwargs.get('game')
+        eval_side = kwargs.get('eval_side')
+        if game.game_status in [GameStatus.WIN1, GameStatus.WIN2]:
+            return game.game_status * eval_side
+        else:
+            return 0
+
--- player.py
+++ player.py
@@ -1,55 +1,17 @@
 # -*- coding=utf8 -*-
 
 import common
-import conf
-import evaluate
 import random
-import search
-from itertools import product
-from game import *
 
 
-class Player(object):
-    def __init__(self, player_idx=None):
-        self.PLAYER = player_idx
-
-    def choose_best_move(self, game, *args):
-        raise NotImplementedError
-
-
-class TicTacToePlayer(Player):
-    def __init__(self, player_idx):
-        super(TicTacToePlayer, self).__init__(player_idx)
-        self.__search = search.MinMax(
-            evaluate.SimpleEndEval(),
-            max_depth=conf.tictactoe_minmax_depth)
-
-    def choose_best_move(self, game, *args):
-        return self.__search.search_best_move(game, eval_side=self.PLAYER)
-
-
-class GoBangPlayer(Player):
-    def __init__(self, player_idx):
-        super(GoBangPlayer, self).__init__(player_idx)
-        self.__search = search.MinMax(
-            evaluate.SimpleEndEval(),
-            max_depth=conf.gobang_minmax_depth)
-
-    def choose_best_move(self, game, *args):
-        common.dprint('considering...')
-        pos, value = self.__search.search_best_move(game, eval_side=self.PLAYER)
-        common.dprint('\t'.join(['my move:', common.pos2h(pos, game.board.width), str(value)]))
-        return pos, value
-
-
-class RandomPlayer(Player):
+class RandomPlayer(object):
     def choose_best_move(self, game, *args):
         avail_moves = game.board.nearby_availables
         value = -999
         return avail_moves[int(random.random()*len(avail_moves))], value
 
 
-class ManualPlayer(Player):
+class ManualPlayer(object):
     def choose_best_move(self, game, *args):
         move = raw_input('Input position, e.g. f11\n')
         pos = common.h2pos(move, game.board.width)
@@ -58,11 +20,3 @@ class ManualPlayer(Player):
             return self.choose_best_move(game)
         value = -999
         return pos, value
-
-
-if __name__ == '__main__':
-    game = TicTacToe()
-    p1 = RandomPlayer()
-    p3 = TicTacToePlayer(-1)
-    print dir(p3)
-    print p3.choose_best_move(game)
